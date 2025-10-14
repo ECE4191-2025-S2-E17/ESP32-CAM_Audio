1e043da2dae9ac10f2bd468cc2fc2bed66f8910f
Matthew Gu <jguu0036@student.monash.edu>
2025-10-14 13:49:41 +1100

    Remove unused http motor server

--- motor_driver.h
+++ motor_driver.h
@@ -1,9 +1,9 @@
 #include <WebServer.h>
 #include <WebSocketsServer.h>
+#include <Wifi.h>
 
 //---- Motor Driver Configuration ------------
 
-WebServer MotorServer(84);
 WebSocketsServer webSocket(85); // WebSocket on port 85
 #define MOTOR_BAUD_RATE 115200
 
@@ -13,7 +13,7 @@ void setupSerial() {
     while (!Serial) {
         ; // wait for serial port to connect. Needed for native USB
     }
-    // Serial.println("Serial interface initialized");
+
 }
 
 //---- WebSocket Event Handler ------------
@@ -30,6 +30,7 @@ void webSocketEvent(uint8_t num, WStype_t type, uint8_t *payload, size_t length)
         // Serial.printf("[%u] Connected from %d.%d.%d.%d\n", num, ip[0], ip[1], ip[2], ip[3]);
         // Send connection confirmation
         webSocket.sendTXT(num, "INFO: Connected to ESP32 Motor Controller");
+        webSocket.sendTXT(num, "INFO: WiFi strength: " + String(WiFi.RSSI()) + "dBm");
         break;
     }
 
@@ -73,7 +74,11 @@ void checkSTM32Messages() {
         // Broadcast STM32 response to all connected WebSocket clients
         if (message.length() > 0) {
             // Serial.println("STM32 Response: " + message);
-            webSocket.broadcastTXT(message);
+            if (message.startsWith("HEARTBEAT")){
+                webSocket.broadcastTXT(message + String(WiFi.RSSI()));
+            } else {
+                webSocket.broadcastTXT(message);
+            }
         }
     }
 }
@@ -81,28 +86,8 @@ void checkSTM32Messages() {
 void motor_websocket_server() {
     webSocket.begin();
     webSocket.onEvent(webSocketEvent);
-
-    // Optional: Keep HTTP server for basic info page
-    MotorServer.on("/", HTTP_GET, []() {
-        String html = "<html><body>";
-        html += "<h1>ESP32 Motor Controller</h1>";
-        html += "<p>WebSocket Server running on port 85</p>";
-        html += "<p>Connect via: ws://" + WiFi.localIP().toString() + ":85</p>";
-        html += "<script>";
-        html += "var ws = new WebSocket('ws://" + WiFi.localIP().toString() + ":85');";
-        html += "ws.onmessage = function(event) { console.log('Received:', event.data); };";
-        html += "function sendCommand(cmd) { ws.send(cmd); }";
-        html += "</script>";
-        html += "<button onclick=\"sendCommand('W,100,50')\">Forward</button>";
-        html += "<button onclick=\"sendCommand('W,0,0')\">Stop</button>";
-        html += "</body></html>";
-        MotorServer.send(200, "text/html", html);
-    });
-
     MotorServer.begin();
-
     Serial.println("Motor WebSocket server started on port 85");
-    Serial.println("HTTP info page: http://[ESP32_IP]:84/");
     Serial.println("WebSocket URL: ws://[ESP32_IP]:85");
 }
 
